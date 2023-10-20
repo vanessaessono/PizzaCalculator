593d88c218c9a2b2349029727e79f78ec3afce96
vanessaessono <gngonoessono@unomaha.edu>
2023-10-19 23:55:05 -0500

    "new commit pizza"

---
+++ PizzaOrderProject/src/MyServletPizDB.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServletPizDB")
+public class MyServletPizDB extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   static String url = "jdbc:mysql://ec2-18-227-52-35.us-east-2.compute.amazonaws.com:3306/myDBPiz";
+   static String user = "vanessaessono_remote";
+   static String password = "Colin0513$";
+   static Connection connection = null;
+
+   public MyServletPizDB() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html;charset=UTF-8");
+      response.getWriter().println("-------- MySQL JDBC Connection Testing ------------<br>");
+      try {
+         Class.forName("com.mysql.cj.jdbc.Driver");//("com.mysql.jdbc.Driver");
+      } catch (ClassNotFoundException e) {
+         System.out.println("Where is your MySQL JDBC Driver?");
+         e.printStackTrace();
+         return;
+      }
+      response.getWriter().println("MySQL JDBC Driver Registered!<br>");
+      connection = null;
+      try {
+         connection = DriverManager.getConnection(url, user, password);
+      } catch (SQLException e) {
+         System.out.println("Connection Failed! Check output console");
+         e.printStackTrace();
+         return;
+      }
+      if (connection != null) {
+         response.getWriter().println("You made it, take control your database now!<br>");
+      } else {
+         System.out.println("Failed to make connection!");
+      }
+      try {
+         String selectSQL = "SELECT * FROM myPizTable";// WHERE MYUSER LIKE ?";
+//         String theUserName = "user%";
+         response.getWriter().println(selectSQL + "<br>");
+         response.getWriter().println("------------------------------------------<br>");
+         PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+//         preparedStatement.setString(1, theUserName);
+         ResultSet rs = preparedStatement.executeQuery();
+         while (rs.next()) {
+            String id = rs.getString("ID");
+            String username = rs.getString("MYUSER");
+            String toppings = rs.getString("TOPPINGS");
+            String size = rs.getString("SIZE");
+            response.getWriter().append("USER ID: " + id + ", ");
+            response.getWriter().append("USER NAME: " + username + ", ");
+            response.getWriter().append("USER TOPPINGS: " + toppings + ", ");
+            response.getWriter().append("PIZZA SIZE: " + size+ "<br>");
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+}
